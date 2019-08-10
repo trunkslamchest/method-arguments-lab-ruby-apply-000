ed39fe86bd6b4baa059472251cb401a5ae3bea74
Austin Smith <trunk9slamchest@gmail.com>
2019-08-10 16:08:13 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,6 +6,6 @@ def introduction_with_language(name, language)
   return "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-puts introduction("Josh")
+# puts introduction("Josh")
 
-puts introduction_with_language("Dan", "Ember.js")
+# puts introduction_with_language("Dan", "Ember.js")
